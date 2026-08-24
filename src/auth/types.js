cb81b78c1b15a8aebf265fe202c284aec7dd60ab
MakarosVar <christomvarv@yahoo.com>
2026-08-24 16:43:45 +0300

    add typing

---
+++ src/auth/types.js
@@ -0,0 +1,48 @@
+/**
+ * @typedef {Object} LoginCredentials
+ * @property {string} email
+ * @property {string} password
+ */
+
+/**
+ * @typedef {Object} Permission
+ * @property {string} id
+ * @property {string} name
+ * @property {string} description
+ */
+
+/**
+ * @typedef {Object} Role
+ * @property {string} id
+ * @property {string} name
+ * @property {string} description
+ * @property {Permission[]} permissions
+ */
+
+/**
+ * @typedef {Object} User
+ * @property {string} id
+ * @property {string} email
+ * @property {string} first_name
+ * @property {string} last_name
+ * @property {string | null} phone
+ * @property {boolean} is_active
+ * @property {Role} role
+ */
+
+/**
+ * @typedef {Object} LoginResponse
+ * @property {string} access_token
+ * @property {User} user
+ */
+
+/**
+ * @typedef {Object} AuthContextValue
+ * @property {User | null} user
+ * @property {string | null} accessToken
+ * @property {boolean} isAuthLoading
+ * @property {(credentials: LoginCredentials) => Promise<User>} login
+ * @property {() => Promise<void>} logout
+ */
+
+export {}
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,13 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './auth/AuthProvider.jsx'
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('Root element not found')
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
