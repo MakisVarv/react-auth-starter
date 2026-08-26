d1c852d0c38e2faf70d47bb116c490cc3a71e53a
MakarosVar <christomvarv@yahoo.com>
2026-08-26 18:44:08 +0300

    Create permissions.js

---
+++ src/auth/permissions.js
@@ -0,0 +1,13 @@
+/** @import { User } from './types.js' */
+
+/**
+ * @param {User | null} user
+ * @param {string} permissionName
+ * @returns {boolean}
+ */
+
+function hasPermission(user, permissionName) {
+  if (user === null) return false
+  return user.role.permissions.some((p) => p.name === permissionName)
+}
+export default hasPermission
