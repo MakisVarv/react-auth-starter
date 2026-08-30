79bec1dd75574cd8410da03764528bc5536316ea
MakarosVar <christomvarv@yahoo.com>
2026-08-30 17:53:44 +0300

    Update userService.js

--- src/features/users/userService.js
+++ src/features/users/userService.js
@@ -17,6 +17,20 @@ export async function getUsers(params, accessToken) {
   return response.data
 }
 /**
+ * @param {string} userId
+ * @param {string} accessToken
+ * @returns {Promise<User>}
+ */
+
+export async function getUser(userId, accessToken) {
+  const response = await apiClient.get(`/users/${userId}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+  return response.data
+}
+/**
  * @param {{
  *   first_name: string,
  *   last_name: string,
@@ -30,6 +44,25 @@ export async function getUsers(params, accessToken) {
  */
 export async function createUser(payload, accessToken) {
   const response = await apiClient.post('/users/', payload, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+  return response.data
+}
+/**
+ * @param {string} userId
+ * @param {{
+ *   first_name?: string,
+ *   last_name?: string,
+ *   email?: string,
+ *   phone?: string | null
+ * }} payload
+ * @param {string} accessToken
+ * @returns {Promise<User>}
+ */
+export async function editUser(userId, payload, accessToken) {
+  const response = await apiClient.patch(`/users/${userId}`, payload, {
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
